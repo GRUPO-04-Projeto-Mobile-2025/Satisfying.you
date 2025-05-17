8abd14073c8bf108fde4915a86394acd70d2f053
Pedro Lucas <pedrolucasmota2005@gmail.com>
2025-05-17 16:29:42 -0300

    Barra Superior

---
+++ src/components/barraSuperior/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
+
+const BarraSuperior = ({nomeTela, onPress}) => {
+  return (
+    <View style={estilos.container}>
+      <TouchableOpacity style={estilos.botao} onPress={onPress}>
+        <Image
+          source={require('../../../public/icons/seta.png')}
+          style={estilos.setaImg}
+          resizeMode="contain"
+        />
+      </TouchableOpacity>
+      <Text style={estilos.texto}>{nomeTela}</Text>
+    </View>
+  );
+};
+
+const estilos = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 56,
+    backgroundColor: '#2B1D62',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
+  botao: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  setaImg: {
+    width: 30,
+    height: 30,
+  },
+  texto: {
+    color: '#fff',
+    fontSize: 18,
+    marginLeft: 8,
+  },
+});
+
+export default BarraSuperior;
---
+++ src/components/modificarPesquisa/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, TextInput, View, StyleSheet} from 'react-native';
+import BarraSuperior from '../barraSuperior';
+
+const ModificarPesquisa = () => {
+  return <BarraSuperior nomeTela="Modificar Pesquisa" />;
+};
+
+const estilos = StyleSheet.create({
+  view: {},
+});
+
+export default ModificarPesquisa;
